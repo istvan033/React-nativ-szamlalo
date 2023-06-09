35189347799bc0484b7f7c4135f2f9105122ecc1
istvan033 <112931949+istvan033@users.noreply.github.com>
2023-06-09 10:20:46 +0200

    React nativ számláló
    Első próbálkozás

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+
+
+
+
 
 export default function App() {
+  const [count, setCount] = useState(0)
+  function increment(){
+    setCount (count+1)
+  }
+  function reset(){
+    setCount (0)
+    
+  }
+  function decerment(){
+    if( count>0)
+    {
+      setCount (count-1)
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>
+        Számláló
+      </Text>
+      <Text style={styles.count}>
+        {count}
+      </Text>
+
+      <Button title="+1" onPress={increment}/>
+      <Button title="reset" onPress={reset}/>
+      <Button title="-1" onPress={decerment}/>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +43,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#546',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: 'white',
+    fontSize: 32,
+  },
+  count:{
+    color: 'green',
+    fontSize: 75,
+  },
+
 });
